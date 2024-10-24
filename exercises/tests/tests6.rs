5ea10c8aaf81f79f08f3b96bb2519fd929b0a89b
jo_pillar <1833528367@qq.com>
2024-10-24 14:31:10 +0800

    update

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+//
 
 struct Foo {
     a: u128,
@@ -20,8 +20,9 @@ struct Foo {
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
     // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { ??? };
-    todo!("The rest of the code goes here")
+    let mut ret: Box<Foo> = unsafe { Box::from_raw(ptr)};
+    ret.b=Some("hello".to_string());
+    ret
 }
 
 #[cfg(test)]
@@ -35,7 +36,9 @@ mod tests {
 
         let ptr_1 = &data.a as *const u128 as usize;
         // SAFETY: We pass an owned box of `Foo`.
-        let ret = unsafe { raw_pointer_to_box(Box::into_raw(data)) };
+       let ret = unsafe { raw_pointer_to_box(Box::into_raw(data)) };
+        // let ptr=Box::into_raw(data);
+        // let ret=unsafe{ Box::from_raw(ptr)};
 
         let ptr_2 = &ret.a as *const u128 as usize;
 
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,20 +27,23 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
+// 
 
 extern "Rust" {
+    #[link_name = "my_demo_function"]
     fn my_demo_function(a: u32) -> u32;
+    
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
-    // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    // 将函数标记为不进行名称修饰，以便外部链接可以正确引用
+    #[no_mangle]
+    pub fn my_demo_function(a: u32) -> u32 {
         a
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
